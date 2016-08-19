06c25db89aee9189a94e1965a44e4ecea8a9c81b
Svetlana Malikova <s.u.malikova@gmail.com>
2016-08-19 15:40:44 +0300

    Запрет прокрутки страницы

--- ping-pong.js
+++ ping-pong.js
@@ -329,3 +329,14 @@ if (animFrame !== null) {
 	setInterval(mainloop, ONE_FRAME_TIME);
 }
 
+//запрет прокрутки страницы
+document.onmousewheel = document.onwheel = function() { 
+	return false;
+};
+document.addEventListener("MozMousePixelScroll", function() {return false}, false);
+document.onkeydown = function(e) {
+	if ((e.keyCode >= 33) && (e.keyCode <=40)) {
+		return false;
+	} 
+}
+
